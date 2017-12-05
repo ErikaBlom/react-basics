999f057aef591db87c210129bf9869e705b77a53
ErikaBlom <ebl@qdelft.nl>
2017-12-05 10:24:00 +0100

    First assignment : show list

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,37 @@ import React, { Component } from 'react';
 import logo from '../QDelft_logo.svg';
 import '../../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../styles/App.css';
+import {todos} from '../data/todoData.js';
+
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App container">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">QDelft Basic React To Do</h1>
-        </header>
-        <div className="row">
-          <div className="col-xs-12">
-            <p className="App-intro">Your ToDo application will go here</p>
-          </div>
+
+    componentWillMount()
+    {
+        this.setState({todos});
+    }
+
+
+
+    render() {
+
+    const todoList = todos.map(todo => <li>{todo.heading} - {todo.detail}</li>);
+
+        return (
+
+        <div className="App container">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">QDelft Basic React To Do</h1>
+            </header>
+            <div className="row">
+                <div className="col-xs-12">
+                <p className="App-intro">Your ToDo application will go here</p>
+              <ul>
+                  {todoList}
+              </ul>
+            </div>
         </div>
       </div>
     );
